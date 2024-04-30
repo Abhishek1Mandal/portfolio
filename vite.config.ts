2ff3687b0d3c43c7fbf9a4177cfc2a184f3c56c8
Abhishek1Mandal <rozzabhishekmandal@gmail.com>
2024-04-30 12:23:22 +0530

    Update on vite confi

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
+  base: "/portfolio",
   mode: 'production',
   plugins: [react()],
   resolve: {
